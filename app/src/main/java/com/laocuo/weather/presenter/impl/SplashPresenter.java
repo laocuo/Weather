c3219c955d2022911e41bd5a2109d24a1c7c7aad
cheng.zhao-nb <cheng.zhao-nb@jrdcom.com>
2016-09-23 15:07:09 +0800

    commit diectory

---
+++ app/src/main/java/com/laocuo/weather/presenter/impl/SplashPresenter.java
@@ -0,0 +1,20 @@
+package com.laocuo.weather.presenter.impl;
+
+
+import com.laocuo.weather.presenter.model.SplashInterface;
+
+/**
+ * Created by hoperun on 9/23/16.
+ */
+
+public class SplashPresenter {
+
+    private SplashInterface mView;
+    public void setView(SplashInterface view) {
+        mView = view;
+    }
+
+    public void checkNetWork() {
+
+    }
+}
---
+++ app/src/main/java/com/laocuo/weather/presenter/model/SplashInterface.java
@@ -0,0 +1,10 @@
+package com.laocuo.weather.presenter.model;
+
+/**
+ * Created by hoperun on 9/23/16.
+ */
+
+public interface SplashInterface {
+    void showEnter();
+    void showError();
+}
