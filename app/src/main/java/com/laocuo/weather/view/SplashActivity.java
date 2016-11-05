bb02a2d0fb4fd317440e1f8a76637ab523103766
cheng.zhao-nb <cheng.zhao-nb@jrdcom.com>
2016-11-05 11:12:26 +0800

    license

--- app/src/main/java/com/laocuo/weather/view/SplashActivity.java
+++ app/src/main/java/com/laocuo/weather/view/SplashActivity.java
@@ -18,6 +18,21 @@ import com.laocuo.weather.presenter.impl.SplashPresenter;
 import com.laocuo.weather.presenter.model.ISplashInterface;
 
 /**
+ Copyright (C) laocuo <laocuo@163.com>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+/**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
